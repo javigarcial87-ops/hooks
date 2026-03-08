ee1b4e1904998b67a56d8d455c2b187870844bf7
javier garcia alvarez <javigarcial87@gmail.com>
2026-03-08 21:09:43 +0100

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
+import { useFetcthCharacters} from './hooks/useFecthCharacters';
 import './App.css';
 
-function App() {
-  const urlPokemon = 'https://pokeapi.co/api/v2/pokemon/1';
+const urlPokemon = 'https://pokeapi.co/api/v2/pokemon/1';
   const urlRick = 'https://rickandmortyapi.com/api/character/1';
+
+
+  function App() {
+  const pokemon = useFetcthCharacters(urlPokemon)
+  const rick = useFetcthCharacters(urlRick)
+
+  if(!pokemon||!rick) return <p>Cargando...</p>
   
   return (
     <>
+    <h2>Pokemon</h2>
+    <p>{pokemon.name}</p>
+    <img src={pokemon.sprites.front_default} />
+
+    <h2>Rick & Morty</h2>
+    <p>{rick.name}</p>
+    <img src={rick.image} />
     </>
   );
 }
